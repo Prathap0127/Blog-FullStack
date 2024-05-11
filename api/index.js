78363d8fc11605770f8029e6c731960698c11fb0
Prathap Kumaravel <50357476+Prathap0127@users.noreply.github.com>
2024-05-11 13:25:50 +0530

    Connect to DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+//env config
+dotenv.config();
+
+//connect to DB
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Database is Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 app.listen(3000, () => {
